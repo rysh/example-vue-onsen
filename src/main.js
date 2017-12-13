31c3bdc7eda2f6006f0623d57e6124001cb3e01a
r.ishibashi <ryuhei.ishibashi@bizreach.co.jp>
2017-12-13 16:18:44 +0900

    install onsen-ui (TODO import css)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import 'onsenui/css/onsenui.css';
+// import 'onsenui/css/onsen-css-components.css';
 import Vue from 'vue';
+import VueOnsen from 'vue-onsenui';
 import App from './App';
 import router from './router';
 
+// (3) そして使用します
+Vue.use(VueOnsen);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
